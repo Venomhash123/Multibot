a9d27d85e088e28c6614acb2d6346b76339c7254
Venomhash123 <89146099+Venomhash123@users.noreply.github.com>
2025-02-17 07:00:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from pyrogram.types import Message
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 Client = Client(
     "multibots",
-    bot_token = "7422590172:AAGtz-B1EW6WChkk37kTqWRlE6w8isFcXl4",
+    bot_token = "5598160444:AAEpXtNmrHzquJTAZB3KRYFkj3K_-p15eXo",
     api_id = 18860540,
     api_hash = "22dd2ad1706199438ab3474e85c9afab"
     )
